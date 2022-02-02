2ea537dc8f4e2373a99c87f6106062bc211ec9f7
Ramya <Ramya@DESKTOP-D5EU9LC>
2022-02-02 21:49:06 +0530

    addition program

---
+++ Java_Demo_Class/src/Addition.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Addition {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Enter a and b: ");
+		Scanner sc= new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println("addition: "+(a+b));
+
+	}
+
+}
